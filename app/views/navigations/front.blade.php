a324b6c33f9d24582cb5cd2b4b5e047d18075ca2
Dominikus D <dikomunikasikan@gmail.com>
2015-05-03 22:30:01 +0700

    no message

--- app/views/navigations/front.blade.php
+++ app/views/navigations/front.blade.php
@@ -1,82 +1,111 @@
 <div class="row s_top_nav">
-	<div class="g-xs-12">
+	<div class="col-xs-12"> 
+		<a href="">
+			home -> image
+		</a>
 
-		<ul style="margin-bottom: 0px;">
+		<ul class="s_first_level_menu">
 			<li class="dropdown">
-				<a href="#" class="dropdown-toggle s_about_us_link" data-toggle="dropdown">about us
-          <!-- <span class="s_in_au_bg"></span> -->
-        </a>
-				<ul class="dropdown-menu s_about_us_submenu" role="menu" style="min-width: 125px !important; left: -3px;">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">BDYD 2015 
+				</a>
+				<ul class="dropdown-menu" role="menu" style="margin-top: -5px;">
 					<li>
-						<a href="{{URL::to('about_us')}}" class="s_au_child_link" style="margin: 0px; width: 125px; padding: 0px; line-height: 50px;">Who We Are?</a>
+						<a href="" class="" style="">Logo dan Tema</a>
 					</li>
 					<li>
-						<a href="{{URL::to('our_services')}}" class="s_au_child_link" style="margin: 0px; width: 125px; padding: 0px; line-height: 50px;">Our Service</a>
+						<a href="" class="" style="">Venue</a>
 					</li>
-
+					<li>
+						<a href="" class="" style="">Pre-event</a>
+					</li>
+					<li>
+						<a href="" class="" style="">Jadwal</a>
+					</li>
+					<li>
+						<a href="" class="" style="">Panitia</a>
+					</li>
+					<li>
+						<a href="" class="" style="">Doa BDYD 2015</a>
+					</li> 
+				</ul>
+			</li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">About BDYD 
+				</a>
+				<ul class="dropdown-menu" role="menu" style="margin-top: -5px;">
+					<li>
+						<a href="" class="" style="">Sejarah BDYD</a>
+					</li>
+					<li>
+						<a href="" class="" style="">Tujuan BDYD</a>
+					</li>
+					<li>
+						<a href="" class="" style="">Theme Song BDYD</a>
+					</li> 
+				</ul>
+			</li>
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Past BDYD 
+				</a>
+				<ul class="dropdown-menu" role="menu" style="margin-top: -5px;">
+					<li>
+						<a href="" class="" style="">BDYD 2011</a>
+					</li>
+					<li>
+						<a href="" class="" style="">BDYD 2013</a>
+					</li> 
 				</ul>
 			</li>
 			<li>
-				<a href="{{URL::to('showcase')}}">showcase</a>
+				<a href="">Sponsor dan Donatur</a>
 			</li>
 			<li>
-				<a href="{{URL::to('home')}}" class="s_home_link">alexindo perkasa
-        <span class="s_interior_sign">
-          INTERIOR
-        </span>
-        </a>
+				<a href="">Gallery</a>
 			</li>
 			<li>
-				<a href="{{URL::to('material')}}">material</a>
-			</li>
-			<li>
-				<a href="{{URL::to('contact_us')}}">contact us </a>
+				<a href="">Pendaftaran</a>
 			</li>
 		</ul>
 	</div>
 </div>
 <script>
- // $('.dropdown').on('show.bs.dropdown', function(e){
- //    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
- //  });
-
-(function ($, window, delay) {
+	(function ($, window, delay) {
   // http://jsfiddle.net/AndreasPizsa/NzvKC/
   var theTimer = 0;
   var theElement = null;
-    var theLastPosition = {x:0,y:0};
+  var theLastPosition = {x:0,y:0};
   $('[data-toggle]')
-    .closest('li')
-    .on('mouseenter', function (inEvent) {
-    if (theElement) theElement.removeClass('open');
-    window.clearTimeout(theTimer);
-    theElement = $(this);
+  .closest('li')
+  .on('mouseenter', function (inEvent) {
+  	if (theElement) theElement.removeClass('open');
+  	window.clearTimeout(theTimer);
+  	theElement = $(this);
 
-    theTimer = window.setTimeout(function () {
-      theElement.addClass('open');
-    }, delay);
+  	theTimer = window.setTimeout(function () {
+  		theElement.addClass('open');
+  	}, delay);
   })
-    .on('mousemove', function (inEvent) {
-        if(Math.abs(theLastPosition.x - inEvent.ScreenX) > 4 || 
-           Math.abs(theLastPosition.y - inEvent.ScreenY) > 4)
-        {
-            theLastPosition.x = inEvent.ScreenX;
-            theLastPosition.y = inEvent.ScreenY;
-            return;
-        }
-        
-    if (theElement.hasClass('open')) return;
-    window.clearTimeout(theTimer);
-    theTimer = window.setTimeout(function () {
-      theElement.addClass('open');
-    }, delay);
+  .on('mousemove', function (inEvent) {
+  	if(Math.abs(theLastPosition.x - inEvent.ScreenX) > 4 || 
+  		Math.abs(theLastPosition.y - inEvent.ScreenY) > 4)
+  	{
+  		theLastPosition.x = inEvent.ScreenX;
+  		theLastPosition.y = inEvent.ScreenY;
+  		return;
+  	}
+  	
+  	if (theElement.hasClass('open')) return;
+  	window.clearTimeout(theTimer);
+  	theTimer = window.setTimeout(function () {
+  		theElement.addClass('open');
+  	}, delay);
   })
-    .on('mouseleave', function (inEvent) {
-    window.clearTimeout(theTimer);
-    theElement = $(this);
-    theTimer = window.setTimeout(function () {
-      theElement.removeClass('open');
-    }, delay);
+  .on('mouseleave', function (inEvent) {
+  	window.clearTimeout(theTimer);
+  	theElement = $(this);
+  	theTimer = window.setTimeout(function () {
+  		theElement.removeClass('open');
+  	}, delay);
   });
 })(jQuery, window, 100); // 200 is the delay in milliseconds
 </script>

8187b7ed8d974c39f144b6c0aeb55668bd955fcb
andreas krisandy <andreaskrisandy@yahoo.co.id>
2015-05-16 11:52:31 +0700

    home gambar kosong

--- app/views/pages/front/home/home.blade.php
+++ app/views/pages/front/home/home.blade.php
@@ -4,6 +4,49 @@
 <section class="s_top_nav_cntr">
 	<div class="container-fluid">
 		@include('navigations.front')
+
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="4.jpg" alt="OMK">
+    </div>
+
+    <div class="item">
+      <img src="4.jpg" alt="OMK">
+    </div>
+
+    <div class="item">
+      <img src="4.jpg" alt="OMK">
+    </div>
+
+    <div class="item">
+      <img src="4.jpg" alt="OMK">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>	
+
+
+
+
 	</div>
 </section>
 
@@ -13,6 +56,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-10 col-xs-push-1">
+
 				
 				<h2>Pendahuluan</h2>
 				<p><B>Bandung Diocese Youth Day</B>,atau biasa disingkat BDYD merupakan Hari Orang Muda Katolik (OMK) se-Keuskupan Bandung yang diselenggarakan oleh Komisi Kepemudaan Keuskupan Bandung. BDYD merupakan suatu perayaan iman, saat OMK Keuskupan Bandung yang berasal dari berbagai paroki dan kategorial berkumpul bersama, saling mengenal, meneguhkan, dan berbagi satu sama lain. 
@@ -36,10 +80,8 @@
 		<div class="row">
 			<div class="col-xs-10 col-xs-push-1">
 				
-				<h2>Pre-Event</h2>
-				<p><b>Bagaimana cara mendaftar pre-event ?</b></p>
-				<p>
-				</p>
+			
+				
 			</div>
 		</div>
 	</div>
